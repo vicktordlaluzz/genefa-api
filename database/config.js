a3c6b880729327aea2fdb3f96a9004f84d0acda6
vicktordlaluzz <vicktordlaluzz@gmail.com>
2021-06-27 14:34:58 -0500

    Preparando Deploy

--- database/config.js
+++ database/config.js
@@ -7,7 +7,7 @@ const dbConnection = async() => {
         // process.env.DB_CON
         // LOCAL
         // process.env.DB_CON_LOCAL
-        await mongoose.connect(process.env.DB_CON_LOCAL, {
+        await mongoose.connect(process.env.DB, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
